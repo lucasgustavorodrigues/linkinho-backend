216361c45e438b7f8506f9934df8340651598f5c
Lucas Gustavo Rodrigues <lg.rodrigues@outlook.com>
2025-09-13 08:50:56 -0300

    test: add tests for handling invalid body and missing body in shorten controller

--- src/controllers/shorten-controller.test.ts
+++ src/controllers/shorten-controller.test.ts
@@ -3,9 +3,9 @@ import type { HttpRequest } from "../types/http";
 import { ShortenController } from "./shorten-controller";
 
 describe("Shorten Controller", () => {
-	it("should return badRequest (400) if the url is invalid", async () => {
+	it("should return badRequest (400) if the body property name is incorrect", async () => {
 		const request: HttpRequest = {
-			body: { url: "invalid-url" },
+			body: { url: "https://www.google.com" },
 			params: {},
 			queryParams: {},
 		};
@@ -14,5 +14,42 @@ describe("Shorten Controller", () => {
 
 		expect(response.statusCode).toBe(400);
 		expect(response.body).toHaveProperty("errors");
+	});
+
+	it("should return badRequest (400) if the url is invalid", async () => {
+		const request: HttpRequest = {
+			body: { longUrl: "invalid-url" },
+			params: {},
+			queryParams: {},
+		};
+
+		const response = await ShortenController.handle(request);
+
+		expect(response.statusCode).toBe(400);
+		expect(response.body).toHaveProperty("errors");
+	});
+
+	it("should return 201 for valid URL", async () => {
+		const request: HttpRequest = {
+			body: { longUrl: "https://www.google.com" },
+			params: {},
+			queryParams: {},
+		};
+
+		const response = await ShortenController.handle(request);
+
+		expect(response.statusCode).toBe(201);
+		expect(response.body).toHaveProperty("shortUrl");
+	});
+
+	it("should handle missing body", async () => {
+		const request: HttpRequest = {
+			body: {},
+			params: {},
+			queryParams: {},
+		};
+
+		const response = await ShortenController.handle(request);
+		expect(response.statusCode).toBe(400);
 	});
 });
